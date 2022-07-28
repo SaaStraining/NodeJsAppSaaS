f60b9d5c926ac76d8377bf898a199353de71f751
AnassNebdaoui <nebdaoui.anass@gmail.com>
2022-07-28 09:03:18 +0100

    checking api with mysql insert

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+const mysql = require('mysql');
+const conn = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'npm_saas'
+});
+
+
+
+
 app.get("/",(req,res) =>{
-    res.send("Hello Test")
+    const insertEmp = "INSERT INTO `employe_test` (`id`, `statut`, `nom`, `prenom`, `cin`, `tel`, `mail`, `addresse`, `sb`, `cs`, `sn`) VALUES (NULL, 'Dqqr', 'Nebdaoui', 'Anass', 'Q340834', '0708071759', 'nebdaoui.anass@gmail.com', '16 etc', '12.33113', '311.31', '311.13')";
+    conn.query(insertEmp, (err,result) =>{
+        res.send("Hello Test port 3001")
+
+    })
 });
 
 app.listen(3001,() =>{
