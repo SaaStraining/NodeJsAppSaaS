1a64b0177f682bea0a458f76b3c703a31265ccd5
AnassNebdaoui <nebdaoui.anass@gmail.com>
2022-07-29 04:51:13 +0100

    Manually insert MySql test

--- index.js
+++ index.js
@@ -9,15 +9,12 @@ const conn = mysql.createPool({
     database:'npm_saas'
 });
 
-
-
-
 app.get("/",(req,res) =>{
-    const insertEmp = "INSERT INTO `employe_test` (`id`, `statut`, `nom`, `prenom`, `cin`, `tel`, `mail`, `addresse`, `sb`, `cs`, `sn`) VALUES (NULL, 'Dqqr', 'Nebdaoui', 'Anass', 'Q340834', '0708071759', 'nebdaoui.anass@gmail.com', '16 etc', '12.33113', '311.31', '311.13')";
-    conn.query(insertEmp, (err,result) =>{
-        res.send("Hello Test port 3001")
+    // const insertEmp = "INSERT INTO `employe_test` (`id`, `statut`, `nom`, `prenom`, `cin`, `tel`, `mail`, `addresse`, `sb`, `cs`, `sn`) VALUES (NULL, 'Dqqr', 'Nebdaoui', 'Anass', 'Q340834', '0708071759', 'nebdaoui.anass@gmail.com', '16 etc', '12.33113', '311.31', '311.13')";
+    // conn.query(insertEmp, (err,result) =>{
+    //     res.send("Hello Test port 3001")
 
-    })
+    // })
 });
 
 app.listen(3001,() =>{
